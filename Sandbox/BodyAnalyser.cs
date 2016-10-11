c254092c84fd6ca0dfd04713c2f10cedce96f9f4
anne55x9 <anne55x9@edu.easj.dk>
2016-10-11 18:19:33 +0200

    16 done

--- Sandbox/BodyAnalyser.cs
+++ Sandbox/BodyAnalyser.cs
@@ -19,7 +19,28 @@ namespace Sandbox
         public String AnalyseBMI(double heightInMeters, double weightInKilo)
         {
             // The below code must be changed
-            String analysis = "All people are beautiful, YOU are beautiful!";
+            String analysis = "";
+            if (CalculateBMI(heightInMeters, weightInKilo) <= 15)
+            {
+                analysis = "You are way too skinny!";
+            }
+            else if(CalculateBMI(heightInMeters, weightInKilo) > 15 && CalculateBMI(heightInMeters,weightInKilo) <= 22)
+            {
+                analysis = "You are a bit skinny!";
+            }
+            else if(CalculateBMI(heightInMeters,weightInKilo) > 22 && CalculateBMI(heightInMeters,weightInKilo) <= 28)
+            {
+                analysis = "You are just fine!";
+            }
+            else if(CalculateBMI(heightInMeters,weightInKilo) > 28 && CalculateBMI(heightInMeters,weightInKilo) <= 35)
+            {
+                analysis = "You are a bit overweight!";
+            }
+            
+            else
+            {
+                analysis = "You are way too fat!";
+            } 
 
             return analysis;
         }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,24 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            BodyAnalyser Body1 = new BodyAnalyser();
+
+           
+            Console.WriteLine($"resultat af bmi body1 {Body1.CalculateBMI(1.5, 30 )}");
+            Console.WriteLine($"bmi analyse body1 {Body1.AnalyseBMI(1.5,30)}");
+
+            BodyAnalyser Body2 = new BodyAnalyser();
+
+            Console.WriteLine($"resultat af bmi body2 {Body2.CalculateBMI(1.9, 90)}");
+            Console.WriteLine($"bmi analyse af body2 {Body2.AnalyseBMI(1.9,90)}");
+
+            BodyAnalyser Body3 = new BodyAnalyser();
+
+            Console.WriteLine($"resultat af bmi body3 {Body3.CalculateBMI(1.7, 200)}");
+            Console.WriteLine($"bmi analyse af body3 {Body3.AnalyseBMI(1.7, 200)}");
+
+          
+
             // The LAST line of code should be ABOVE this line
         }
     }
